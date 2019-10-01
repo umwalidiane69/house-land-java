af119fbb4405d40af4a75dbfb2e6da4b24d08318
GraceUmutesi <tesigrace98@gmail.com>
2019-10-01 10:59:28 +0200

    add changes

--- src/test/java/Databaserule.java
+++ src/test/java/Databaserule.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import org.junit.rules.ExternalResource;
+import org.sql2o.*;
 
-public class Databaserule {
+class DatabaseRule extends ExternalResource {
+
+    @Override
+    protected void before() {
+//        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/virtual_pets_test", "wecode", "1234");  //Those with linux or windows use two strings for username and password
+    }
 }
