e349d9644e02f02dc22e5d282bf95f52d3f13d0c
GraceUmutesi <tesigrace98@gmail.com>
2019-10-01 11:08:57 +0200

    add changes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,16 @@
+import static spark.Spark.*;
+import java.util.HashMap;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import java.util.List;
+import java.util.Map;
+import spark.ModelAndView;
+
 public class App {
+    public static void main(String [] args){
+        staticFileLocation("/public");
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
 }
